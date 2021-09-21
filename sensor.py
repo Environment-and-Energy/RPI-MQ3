98c1f503116256bb9f26ab0603bbc41261a59ed1
Matt <heffa2004@gmail.com>
2021-09-21 21:39:04 +1000

    added python code for sensor

--- sensor.py
+++ sensor.py
@@ -1,20 +1,26 @@
-#define MQ3pin 0
+# MQ3 SENSOR CODE
 
-#Arduino script, need to find Python Equivalent
+""" 
+If you have not installed the Wiring Pi Library,
+install it using below command : 
 
-float sensorValue;  //variable to store sensor value
+sudo pip install wiringpi
 
-void setup() {
-	Serial.begin(9600); // sets the serial port to 9600
-	Serial.println("MQ3 warming up!");
-	delay(20000); // allow the MQ3 to warm up
-}
+"""
 
-void loop() {
-	sensorValue = analogRead(MQ3pin); // read analog input pin 0
+#Copy and Paste the below code and save it as a ".py" file
 
-	Serial.print("Sensor Value: ");
-	Serial.println(sensorValue);
-	
-	delay(2000); // wait 2s for next reading
-}
+import wiringpi as wiringpi
+from time import sleep
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(25, 0)
+count=0
+while(count<20):
+	my_input=wiringpi.digitalRead(25)
+	if(my_input):
+		print("Not Detected !")
+	else:
+		print("Alcohol Detected")
+	count=count+1
+	sleep(1)
