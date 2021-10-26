951242a48130f1f9627612c7df870e865af16a78
MattHeffNT <heffa2004@gmail.com>
2021-10-26 21:01:55 +1100

    working changes for rpi3 and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 Alcohol Sensor
 
 Source code from: "insert attribution"
+
+
+---
+
+## to do
+
+- we need to set a constant distance that the rover will need to travel for demo day, then measure how long it takes to travel that distance (measure approx 5 times to get average time)
+
+- the time it takes to travel the line will be how long we set the sensor to measure.....then we can subtract the max reading index seconds from the total and get the glug rover to travel backwards for the total of that value. 
+
+- add led to show concentration amounts
+
+- figure out how we going to apply filter or ML/AI
--- sensor.py
+++ sensor.py
@@ -1,26 +1,112 @@
-# MQ3 SENSOR CODE
+#!/usr/bin/env python3
 
-""" 
-If you have not installed the Wiring Pi Library,
-install it using below command : 
-
-sudo pip install wiringpi
-
+"""CircuitPython program that reads analog values from an MCP3008 ADC.
+Description
+-----------
+CircuitPython program that reads both single-ended and differential channel
+analog values from an MCP3008 analog-to-digital converter integrated circuit.
+Circuit
+-------
+- MCP3008 ADC connected to the SPI0 serial bus with GPIO22 as the chip select.
+Libraries/Modules
+-----------------
+- adafruit_mcp3xxx CircuitPython Library
+    - https://circuitpython.readthedocs.io/projects/mcp3xxx/
+    - Provides support for MCP3008 analog-to-digital converter.
+- board CircuitPython Core Module
+    - https://circuitpython.readthedocs.io/en/6.0.x/shared-bindings/board/
+    - Access to Raspberry Pi GPIO pins and hardware.
+- busio CircuitPython Core Module
+    - https://circuitpython.readthedocs.io/en/6.0.x/shared-bindings/busio/
+    - Provides SPI serial bus support.
+- digitalio CircuitPython Core Module
+    - https://circuitpython.readthedocs.io/en/6.0.x/shared-bindings/digitalio/
+    - Provides basic digital pin IO support.
+- RPi.GPIO Library (https://pypi.org/project/RPi.GPIO/)
+    - Control and reset GPIO pins.
+- time Standard Library (https://docs.python.org/3/library/time.html)
+    - Access to sleep function.
+Notes
+-----
+- Comments are Sphinx (reStructuredText) compatible.
+TODO
+----
+Author(s)
+---------
+- Created by John Woolsey on 11/09/2020.
+- Modified by John Woolsey on 11/17/2020.
+Copyright (c) 2020 Woolsey Workshop.  All rights reserved.
+Members
+-------
 """
 
-#Copy and Paste the below code and save it as a ".py" file
+# Imports
+import time
+import board
+import busio
+import digitalio
+import adafruit_mcp3xxx.mcp3008 as MCP3008
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import RPi.GPIO
+# import csv
 
-import wiringpi as wiringpi
-from time import sleep
+# Global Constants/Variables/Instances
+SPI = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)  # SPI bus
+MCP3008_CS = digitalio.DigitalInOut(board.D22)  # MCP3008 chip select
+MCP3008_SPI = MCP3008.MCP3008(SPI, MCP3008_CS)  # MCP3008 instance
+ADC_CH0 = AnalogIn(MCP3008_SPI, MCP3008.P0)  # MCP3008 CH0 single channel input
+ADC_CH1 = AnalogIn(MCP3008_SPI, MCP3008.P1)  # MCP3008 CH1 single channel input
+ADC_DIFF = AnalogIn(
+    MCP3008_SPI, MCP3008.P1, MCP3008.P0
+)  # MCP3008 CH1-CH0 differential channel input
 
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(25, 0)
-count=0
-while(count<20):
-	my_input=wiringpi.digitalRead(25)
-	if(my_input):
-		print("Not Detected !")
-	else:
-		print("Alcohol Detected")
-	count=count+1
-	sleep(1)
+
+alcohol_readings = []
+
+
+# Main
+def main():
+    """Main program entry."""
+
+    """Maybe add two minute delay before code starts to allow Alchol sensor to heat up to adequate temperature"""
+
+    # add a heading to each data entry into API
+    with open("data.csv", "w") as fp:
+        # adds a set heading
+        fp.write("alcohol concentration \n")
+        fp.close()
+
+    seconds = 60
+    counter = 0
+    print("Press CTRL-C to exit.")
+    try:
+        while counter != seconds:
+            alcohol = ADC_CH0.value
+            print(alcohol)
+            print()
+            time.sleep(1)
+
+            # push sensor values to array
+            alcohol_readings.append(alcohol)
+            print("this is the array: ", alcohol_readings[counter])
+
+            # function to find index of highest alcohol reading
+            def search(x):
+                maxIndex = x.index(max(x))
+                return (maxIndex)
+            # print index of highest alcohol reading
+            print("max reading is: ", search(alcohol_readings))
+
+            # # write to csv code here
+            # with open("data.csv", "a") as fp:
+            #     fp.write(str(ADC_CH0.value)+"\n")
+            #     time.sleep(1)
+            counter += 1
+            print(f"this is the counter: {counter}")
+    except KeyboardInterrupt:
+        RPi.GPIO.cleanup()  # release all GPIO resources
+        print("\nCleaned up GPIO resources.")
+
+
+if __name__ == "__main__":  # required for generating Sphinx documentation
+    main()
